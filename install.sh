635df6cc5371ea748cdbc574568667a3f8d28f86
Baz Qux <olbegster@gmail.com>
2020-02-02 19:48:33 +0300

    adds fish to install script, removes deprecated tmux

--- install.sh
+++ install.sh
@@ -3,4 +3,11 @@ list=(gitconfig tmux.conf vimrc)
 for i in ${list[@]}; do
     cp ${i} ~/.${i}
 done
+
+mkdir -p ~/.config/fish/functions
+cp config.fish ~/.config/fish
+cp fish_prompt.fish ~/.config/fish/functions
+
+# chsh -s /usr/local/bin/fish
+
 exit 0
